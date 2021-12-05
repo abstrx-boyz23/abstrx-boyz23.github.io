b4567fe34935071b7215e4aec94d856c6cbe28dd
abstrx-boyz23 <afifu5881@gmail.com>
2021-12-05 11:54:06 +0700

    tampilan sudah lumayan dan gak monoton

--- suwit-game-v2/script.js
+++ suwit-game-v2/script.js
@@ -1,82 +1,113 @@
 // <== Suwit Game V.2 ==>
 
 /* Alghoritma :
-1. Tangkap pilihan player 
-2. Tangkap pilihan computer
-3. Buat rules game
-4. Tampilkan outputnya
-*/
+1. Deklarasikan variabel yg dibutuhkan
+    - image computer & player
+    - result
+    - select player
+2. Buat function yg diperlukan
+    - Rules game
+    - Style color result game
+    - Rolling Image Computer*/
 
-// <== Declare Variable ==>
-// Image Computer & Player
+// <<<==== 1. Declare Variable ====>>>
+// a. Image Computer & Player
 const imgComputer = document.getElementById('image-computer');
 const imgPlayer   = document.getElementById('image-player');
-// Rules Suwit Game v.2
+// b. Result Suwit Game v.2
 const result      = document.getElementById('result');
-// Select Player 
-const slcStone    = document.getElementsByClassName('select-stone')[0];
-const slcScissors = document.getElementsByClassName('select-scissors')[0];
-const slcPaper    = document.getElementsByClassName('select-paper')[0];
+// c. Select Player 
+const slcPlayer   = document.querySelectorAll('#selection-of-player img');
 
-// <== Declare Function ==>
-// Select Computer
+
+// <<<==== Declare Function ====>>>
+// a. getResult Game
+const getResult   = (player,computer) => {
+  if (player == computer)   return 'DRAW !!';
+  if (player == 'stone')    return (computer == 'scissors') ? 'YOU WIN !!' : 'COMPUTER WIN !!';
+  if (player == 'scissors') return (computer == 'paper') ? 'YOU WIN !!' : 'COMPUTER WIN !!';
+  if (player == 'paper')    return (computer == 'stone') ? 'YOU WIN !!' : 'COMPUTER WIN !!';
+};
+// b. Style Color Result Game
+const getStyle = (rules) => {
+  if (rules == 'DRAW !!')         return result.style.color = '#c3ad26';
+  if (rules == 'YOU WIN !!')      return result.style.color = '#0bbedb';
+  if (rules == 'COMPUTER WIN !!') return result.style.color = '#f60000';
+};
+// c. Rolling Image Computer
+const rolling = () => {
+  const img = ['stone','scissors','paper'];
+  let i = 0;
+  const timeStart = new Date().getTime()
+  setInterval(() => {
+    if (new Date().getTime() - timeStart > 1500) {
+      clearInterval;
+      return;
+    }
+    imgComputer.setAttribute('src',`image/${img[i++]}.jpg`);
+    if (i == img.length) i = 0;
+  },100)
+};
+
+// <<<==== Catch My Selection ====>>>
+// a. Select Computer
 const getComputer = () => {
   const computer  = Math.random() * 10;
   if (computer <= 3.33)                    return 'stone'; // computer <= 3.33
   if (3.33 < computer && computer <= 6.66) return 'scissors'; // 3.33 < computer <= 6.66
   else                                     return 'paper'; // 6.66 < computer
-}
-
-// Rules Game
-const getResult = (player,computer) => {
-  if (player == computer)   return 'DRAW !!';
-  if (player == 'stone')    return (computer == 'scissors') ? 'YOU WIN !!' : 'COMPUTER WIN !!';
-  if (player == 'scissors') return (computer == 'paper') ? 'YOU WIN !!' : 'COMPUTER WIN !!';
-  if (player == 'paper')    return (computer == 'stone') ? 'YOU WIN !!' : 'COMPUTER WIN !!';
-}
-
-// Style Color Result Game
-const getStyle = (rules) => {
-  if (rules == 'DRAW !!')         return result.style.color = '#c3ad26';
-  if (rules == 'YOU WIN !!')      return result.style.color = '#0bbedb';
-  if (rules == 'COMPUTER WIN !!') return result.style.color = '#f60000';
-} 
-
-// 1. Stone
-slcStone.addEventListener('click', () => {
-  const slcComputer   = getComputer();
-  imgPlayer.src       = 'image/stone.jpg';
-  
-  setTimeout(() => {
-    imgComputer.src   = `image/${slcComputer}.jpg`;
-    result.innerHTML  = getResult('stone',slcComputer)
-    getStyle(getResult('stone',slcComputer));
-  }, 1000);
+};
+// b. Select Player
+slcPlayer.forEach((slc) => {
+  slc.addEventListener('click', () => {
+    const slcComputer   = getComputer();
+    imgPlayer.setAttribute('src',slc.src);
+    
+    rolling();
+    setTimeout(() => {
+      imgComputer.setAttribute('src',`image/${slcComputer}.jpg`);
+      result.innerHTML  = getResult(slc.alt, slcComputer);
+      getStyle(getResult(slc.alt,slcComputer));
+    }, 1500);
+  });
 });
 
-// 2. Scissors
-slcScissors.addEventListener('click', () => {
-  const slcComputer   = getComputer();
-  imgPlayer.src       = 'image/scissors.jpg';
-  
-  setTimeout(() => {
-    imgComputer.src   = `image/${slcComputer}.jpg`;
-    result.innerHTML  = getResult('scissors',slcComputer)
-    getStyle(getResult('scissors',slcComputer));
-  }, 1000);
-});
 
-// 3. Paper
-slcPaper.addEventListener('click', () => {
-  const slcComputer   = getComputer();
-  imgPlayer.src       = 'image/paper.jpg';
+// // 1. Stone
+// slcStone.addEventListener('click', () => {
+//   const slcComputer   = getComputer();
+//   imgPlayer.src       = 'image/stone.jpg';
   
-  setTimeout(() => {
-    imgComputer.src   = `image/${slcComputer}.jpg`;
-    result.innerHTML  = getResult('paper',slcComputer)
-    getStyle(getResult('paper',slcComputer));
-  }, 1000);
-});
+//   setTimeout(() => {
+//     imgComputer.src   = `image/${slcComputer}.jpg`;
+//     result.innerHTML  = getResult('stone',slcComputer)
+//     getStyle(getResult('stone',slcComputer));
+//   }, 1000);
+// });
+
+// // 2. Scissors
+// slcScissors.addEventListener('click', () => {
+//   const slcComputer   = getComputer();
+//   imgPlayer.src       = 'image/scissors.jpg';
+  
+//   setTimeout(() => {
+//     imgComputer.src   = `image/${slcComputer}.jpg`;
+//     result.innerHTML  = getResult('scissors',slcComputer)
+//     getStyle(getResult('scissors',slcComputer));
+//   }, 1000);
+// });
+
+// // 3. Paper
+// slcPaper.addEventListener('click', () => {
+//   const slcComputer   = getComputer();
+//   imgPlayer.src       = 'image/paper.jpg';
+  
+//   setTimeout(() => {
+//     imgComputer.src   = `image/${slcComputer}.jpg`;
+//     result.innerHTML  = getResult('paper',slcComputer)
+//     getStyle(getResult('paper',slcComputer));
+//   }, 1000);
+// });
 
 
 
